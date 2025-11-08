88be37638a80ffe54b6940ce999bc40894a02dc2
Alhajri00 <154027536+Alhajri00@users.noreply.github.com>
2025-11-08 21:27:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-# Guaranteed Bandit hits: B602/B603 (shell=True), B303 (MD5), B605 (os.system)
-import os
-import subprocess
-import hashlib
+# Guaranteed Bandit hits: B605 (os.system), B602/B603 (shell=True), B303 (MD5)
+import os, subprocess, hashlib
 
-# B605: os.system
-os.system("echo from os.system")
-
-# B602/B603: shell=True
-subprocess.call("echo from shell", shell=True)
-
-# B303: weak hash
-print(hashlib.md5(b"hello").hexdigest())
+os.system("echo from os.system")           # B605
+subprocess.call("echo from shell", shell=True)  # B602/B603
+print(hashlib.md5(b"hello").hexdigest())   # B303
